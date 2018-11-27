a815d1305bff0639e14a3b3b625c0e0795aa74dc
MohanNalgire <mnalgire@gmail.com>
2018-11-27 23:07:33 +0530

    updated shared routing file

--- src/app/_sharedModules/app-routing.module.ts
+++ src/app/_sharedModules/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
 import { ProductsDashboardComponent } from '../products-dashboard/products-dashboard.component';
 
 import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
@@ -9,37 +8,37 @@ import { PageNotFoundComponent } from '../page-not-found/page-not-found.componen
 //Routes for only main module
 const routes: Routes = [
   {
-    path:'admin',
-    loadChildren:'../admin/admin.module#AdminModule'
-    },
-  {
-    path:'product',
-    loadChildren:'../product/product.module#ProductModule'
+    path: 'admin',
+    loadChildren: '../admin/admin.module#AdminModule'
   },
   {
-    path:'productDashboard',
-    component:ProductsDashboardComponent
+    path: 'product',
+    loadChildren: '../product/product.module#ProductModule'
   },
   {
-    path:'cart',
-    loadChildren:'../cart/cart.module#CartModule'
+    path: 'productDashboard',
+    component: ProductsDashboardComponent
   },
   {
-    path:'',
-    redirectTo:'productDashboard',
-    pathMatch:'full'
+    path: 'cart',
+    loadChildren: '../cart/cart.module#CartModule'
+  },
+  {
+    path: '',
+    redirectTo: 'productDashboard',
+    pathMatch: 'full'
   },
   {
     path: '**', component: PageNotFoundComponent
-  } 
+  }
 ];
 
 
 @NgModule({
   imports: [
     RouterModule.forRoot(
-      routes,{ enableTracing: true } // <-- debugging purposes only
-      )
+      routes, { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   exports: [RouterModule]
 })
